85520e22969c107440964df44e5a99c7dc0a7d73
Anna Pronina <kiisya1@yandex.ru>
2020-06-06 14:17:59 +0300

    Добавляет функционал отрисовки объявления

--- js/main.js
+++ js/main.js
@@ -115,6 +115,170 @@ var makeMapActive = function () {
   map.classList.remove('map--faded');
 };
 
+// Определяет тип жилья в объявлении
+
+var getHousingType = function (type) {
+  if (type === 'flat') {
+    return 'Квартира';
+  } else if (type === 'bungalo') {
+    return 'Бунгало';
+  } else if (type === 'house') {
+    return 'Дом';
+  } else {
+    return 'Дворец';
+  }
+};
+
+// Скрывает html элемент
+
+var hideHtmlElement = function (htmlElement) {
+  htmlElement.style.display = 'none';
+};
+
+// Добавляет фотографии в объявление
+
+var addPhotosToAd = function (adPhotos, photos) {
+  var adImg = adPhotos.querySelector('.popup__photo');
+  var fragment = document.createDocumentFragment();
+  if (photos.length > 0) {
+    photos.forEach(function (photo, i) {
+      if (i === 0) {
+        adImg.src = photo;
+      } else {
+        var adImgElement = adImg.cloneNode(true);
+        adImgElement.src = photo;
+        fragment.appendChild(adImgElement);
+      }
+    });
+    adPhotos.appendChild(fragment);
+  } else {
+    hideHtmlElement(adPhotos);
+  }
+};
+
+// Добавляет текст в элементы разметки объявления
+
+var addTextToElement = function (htmlElement, text) {
+  if (text) {
+    htmlElement.textContent = text;
+  } else {
+    hideHtmlElement(htmlElement);
+  }
+};
+
+// Добавляет цену в объявление
+
+var addPriceToAd = function (adPrice, price) {
+  if (price) {
+    adPrice.textContent = price + '₽/ночь';
+  } else {
+    hideHtmlElement(adPrice);
+  }
+};
+
+// Добавляет тип жилья в объявление
+
+var addTypeToAd = function (adType, type) {
+  if (type) {
+    adType.textContent = getHousingType(type);
+  } else {
+    hideHtmlElement(adType);
+  }
+};
+
+// Добавляет количество комнат и гостей в объявление
+
+var addСapacityToAd = function (adСapacity, rooms, guests) {
+  if (rooms && guests) {
+    adСapacity.textContent = rooms + ' комнаты для ' + guests + ' гостей';
+  } else {
+    hideHtmlElement(adСapacity);
+  }
+};
+
+// Добавляет время въезда/выезда в объявление
+
+var addTimesToAd = function (adTiming, checkin, checkout) {
+  if (checkin && checkout) {
+    adTiming.textContent = 'Заезд после ' + checkin + ', выезд до ' + checkout;
+  } else {
+    hideHtmlElement(adTiming);
+  }
+};
+
+// Добавляет аватар в объявление
+
+var addAvatarToAd = function (adAvatar, avatar) {
+  if (avatar) {
+    adAvatar.src = avatar;
+  } else {
+    hideHtmlElement(adAvatar);
+  }
+};
+
+// Добавляет удобства в объявление
+
+var addFeaturesToAd = function (adFeatures, features) {
+  if (features.length > 0) {
+    var fragment = document.createDocumentFragment();
+    var featuresItems = adFeatures.querySelectorAll('.popup__feature');
+
+    features.forEach(function (feature) {
+      var clearItem = document.createElement('li');
+      var featureClass = 'popup__feature--' + feature;
+      clearItem.classList.add('popup__feature', featureClass);
+      fragment.appendChild(clearItem);
+    });
+
+    featuresItems.forEach(function (featuresItem) {
+      featuresItem.remove();
+    });
+    adFeatures.appendChild(fragment);
+  } else {
+    hideHtmlElement(adFeatures);
+  }
+};
+
+// Создает карточку объявления
+
+var getAdCard = function (ad) {
+  var adTemplate = document.querySelector('#card')
+      .content
+      .querySelector('.map__card');
+  var adElement = adTemplate.cloneNode(true);
+  var adTitle = adElement.querySelector('.popup__title');
+  var adAdress = adElement.querySelector('.popup__text--address');
+  var adPrice = adElement.querySelector('.popup__text--price');
+  var adType = adElement.querySelector('.popup__type');
+  var adСapacity = adElement.querySelector('.popup__text--capacity');
+  var adTiming = adElement.querySelector('.popup__text--time');
+  var adFeatures = adElement.querySelector('.popup__features');
+  var adDescription = adElement.querySelector('.popup__description');
+  var adPhotos = adElement.querySelector('.popup__photos');
+  var adAvatar = adElement.querySelector('.popup__avatar');
+
+  addTextToElement(adTitle, ad.offer.title);
+  addTextToElement(adAdress, ad.offer.address);
+  addPriceToAd(adPrice, ad.offer.price);
+  addTypeToAd(adType, ad.offer.type);
+  addСapacityToAd(adСapacity, ad.offer.rooms, ad.offer.guests);
+  addTimesToAd(adTiming, ad.offer.checkin, ad.offer.checkout);
+  addTextToElement(adDescription, ad.offer.description);
+  addAvatarToAd(adAvatar, ad.author.avata);
+  addFeaturesToAd(adFeatures, ad.offer.features);
+  addPhotosToAd(adPhotos, ad.offer.photos);
+
+  return adElement;
+};
+
+// Добавляет карточку объявления на страницу
+
+var renderAd = function (ads) {
+  var mapFilterContainer = map.querySelector('.map__filters-container');
+  map.insertBefore(getAdCard(ads[0]), mapFilterContainer);
+};
+
 var ads = generateAdvertisementObjects(NUMBER_OF_ADVERTISEMENT);
 makeMapActive();
 renderPins(ads);
+renderAd(ads);
