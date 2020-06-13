bcc462117cd1d5c2aba2c8fbe41fe74ca502e987
Anna Pronina <kiisya1@yandex.ru>
2020-06-13 17:19:47 +0300

    Добавляет вывод попапа объявления и валидацию полей формы

--- js/main.js
+++ js/main.js
@@ -14,10 +14,10 @@ var MAIN_PIN_WIDTH = 65;
 var MAIN_PIN_HEIGHT = 65;
 var MAIN_PIN_HEIGHT_ACTIVE = 84;
 var LOCATION_X_MIN = 0;
-/*
 var GUESTS_DECLENSIONS = ['гостя', 'гостей'];
 var ROOMS_DECLENSIONS = ['комната', 'комнаты', 'комнат'];
-*/
+var MIN_TITLE_LENGTH = 30;
+var MAX_TITLE_LENGTH = 100;
 
 var map = document.querySelector('.map');
 var locationXMax = map.offsetWidth;
@@ -27,6 +27,11 @@ var adFormFieldsets = adForm.querySelectorAll('fieldset');
 var addressField = adForm.querySelector('#address');
 var roomNumberField = adForm.querySelector('#room_number');
 var capacityField = adForm.querySelector('#capacity');
+var titleField = adForm.querySelector('#title');
+var typeField = adForm.querySelector('#type');
+var priceField = adForm.querySelector('#price');
+var timeIn = adForm.querySelector('#timein');
+var timeOut = adForm.querySelector('#timeout');
 
 var filterForm = document.querySelector('.map__filters');
 var filterSelects = filterForm.querySelectorAll('select');
@@ -115,6 +120,15 @@ var getPinElement = function (ad) {
   return pinElement;
 };
 
+
+// Добавляем обработчик собития на пин
+
+var addPinClickHandler = function (pinElement, ad) {
+  pinElement.addEventListener('click', function () {
+    renderAd(ad);
+  });
+};
+
 // Добавляет метки на карту
 
 var renderPins = function (ads) {
@@ -122,13 +136,13 @@ var renderPins = function (ads) {
   var fragment = document.createDocumentFragment();
 
   ads.forEach(function (ad) {
-    fragment.appendChild(getPinElement(ad));
+    var pinElement = getPinElement(ad);
+    addPinClickHandler(pinElement, ad);
+    fragment.appendChild(pinElement);
   });
 
   pinsBlock.appendChild(fragment);
 };
-
-/*
 
 // Скрывает html элемент
 
@@ -271,6 +285,18 @@ var addNotEmptyContent = function (htmlElement, callback, content, extraContent)
   }
 };
 
+// Закрывает карточку объявления по нажатию Esc
+
+var onAdEscape = function (evt) {
+  if (evt.key === 'Escape') {
+    var adElement = map.querySelector('.map__card');
+    adElement.remove();
+
+    document.removeEventListener('keydown', onAdEscape);
+  }
+};
+
+
 // Создает карточку объявления
 
 var getAdCard = function (ad) {
@@ -288,6 +314,7 @@ var getAdCard = function (ad) {
   var adDescription = adElement.querySelector('.popup__description');
   var adPhotos = adElement.querySelector('.popup__photos');
   var adAvatar = adElement.querySelector('.popup__avatar');
+  var adClose = adElement.querySelector('.popup__close');
 
   addNotEmptyContent(adTitle, addTextToElement, ad.offer.title);
   addNotEmptyContent(adAddress, addTextToElement, ad.offer.address);
@@ -300,19 +327,31 @@ var getAdCard = function (ad) {
   addNotEmptyContent(adFeatures, addFeaturesToAd, ad.offer.features);
   addNotEmptyContent(adPhotos, addPhotosToAd, ad.offer.photos);
 
+  adClose.addEventListener('click', function () {
+    adElement.remove();
+  });
+
+  document.addEventListener('keydown', onAdEscape);
+
   return adElement;
+};
+
+// Удаляет карточку объявления, если она уже есть
+
+var removeAd = function (adElement) {
+  if (adElement) {
+    adElement.remove();
+  }
 };
 
 // Добавляет карточку объявления на страницу
 
 var renderAd = function (ad) {
   var mapFiltersContainer = map.querySelector('.map__filters-container');
+  var existingAdCard = map.querySelector('.map__card');
+  removeAd(existingAdCard);
   map.insertBefore(getAdCard(ad), mapFiltersContainer);
 };
-
-renderAd(ads[0]);
-
-*/
 
 // Переводит карту в активное состояние
 
@@ -359,7 +398,7 @@ var makeItemsDisabled = function (items) {
 var setPinAddress = function (pinWidth, pinHeight) {
   var left = mainPin.style.left;
   var top = mainPin.style.top;
-  addressField.disabled = true;
+  addressField.readOnly = true;
   addressField.value = Math.round(+left.slice(0, left.length - 2) + pinWidth / 2) + ', ' + Math.round(+top.slice(0, top.length - 2) + pinHeight);
 };
 
@@ -375,21 +414,119 @@ var removeBorder = function (formElement) {
   formElement.style.border = 'none';
 };
 
+// Добавляет рамку и сообщение невалидному полю
+
+var setInvalidMode = function (element, message) {
+  addRedBorder(element);
+  element.setCustomValidity(message);
+};
+
+// Убирает рамку и сообщение у валидного поля
+
+var setValidMode = function (element) {
+  removeBorder(element);
+  element.setCustomValidity('');
+};
+
+// Проверяет длину заголовка на соответвие требованиям
+
+var onTitleInput = function (evt) {
+  var valueLength = evt.target.value.length;
+  evt.target.reportValidity();
+  var message;
+  if (valueLength < MIN_TITLE_LENGTH) {
+    message = 'Минимальная длина заголовка: 30 символов. Введите еще ' + (MIN_TITLE_LENGTH - valueLength) + ' симв.';
+    setInvalidMode(evt.target, message);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    message = 'Максимальная длина заголовка: 100 символов. Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) + ' симв.';
+    setInvalidMode(evt.target, message);
+  } else if (valueLength === 0) {
+    message = 'Поле "Заголовок" не может быть пустым';
+    setInvalidMode(evt.target, message);
+  } else {
+    setValidMode(evt.target);
+  }
+};
+
+
+// Синхронизирует поле Время заезда с полем Время выезда
+
+var onTimeInChange = function (evt) {
+  var optionsTimeOut = timeOut.querySelectorAll('option');
+  for (var i = 0; i < optionsTimeOut.length; i++) {
+    if (optionsTimeOut[i].value === evt.target.value) {
+      optionsTimeOut[i].selected = true;
+    }
+  }
+};
+
+// Синхронизирует поле Время выезда с полем Время заезда
+
+var onTimeOutChange = function (evt) {
+  var optionsTimeIn = timeIn.querySelectorAll('option');
+  for (var i = 0; i < optionsTimeIn.length; i++) {
+    if (optionsTimeIn[i].value === evt.target.value) {
+      optionsTimeIn[i].selected = true;
+    }
+  }
+};
+
+// Проверяет соответсвие значений в поле тип жилья и стоимость за ночь
+
+var onTypeAndPriceChange = function () {
+  var typePriceMap = {
+    'bungalo': {
+      minValue: 0,
+      errorText: ''
+    },
+    'flat': {
+      minValue: 1000,
+      errorText: 'Стоимость жилья типа "Квартира" не может быть меньше 1 000 руб. за ночь'
+    },
+    'house': {
+      minValue: 5000,
+      errorText: 'Стоимость жилья типа "Дом" не может быть меньше 5 000 руб. за ночь'
+    },
+    'palace': {
+      minValue: 10000,
+      errorText: 'Стоимость жилья типа "Дворец" не может быть меньше 10 000 руб. за ночь'
+    }
+  };
+  var type = typeField.value;
+  var price = priceField.value;
+  var message;
+  priceField.placeholder = typePriceMap[type].minValue;
+  priceField.reportValidity();
+
+  if (!price) {
+    message = 'Поле "Цена за ночь" не может быть пустым';
+    setInvalidMode(priceField, message);
+  } else if (typePriceMap[type].minValue > price) {
+    setInvalidMode(priceField, typePriceMap[type].errorText);
+    priceField.reportValidity();
+  } else if (typePriceMap[type].minValue < price < priceField.max) {
+    setValidMode(priceField);
+  } else if (price > priceField.max) {
+    message = 'Стоимость жилья за ночь не должна превышат 1 000 000 руб.';
+    setInvalidMode(priceField, message);
+  }
+};
+
 // Проверяет соответсвие значения в поле выбора количества гостей значению количества комнат
 
 var onRoomsAndCapacityChange = function () {
   var roomsCapacityMap = {
     '1': {
       guests: ['1'],
-      errorText: 'В одну комнату можно поселить только одного жильца'
+      errorText: 'В одну комнату можно поселить только одного гостя'
     },
     '2': {
       guests: ['1', '2'],
-      errorText: 'В две комнаты можно поселить только одного или двух жильцов'
+      errorText: 'В две комнаты можно поселить только одного или двух гостей'
     },
     '3': {
       guests: ['1', '2', '3'],
-      errorText: 'Три комнаты могут вместить 1, 2 или 3 жильцов. Значение "не для гостей" недопустимо'
+      errorText: 'Три комнаты могут вместить 1, 2 или 3 гостей. Значение "не для гостей" недопустимо'
     },
     '100': {
       guests: ['0'],
@@ -399,12 +536,11 @@ var onRoomsAndCapacityChange = function () {
   var roomsCount = roomNumberField.value;
   var guestsCount = capacityField.value;
   if (!roomsCapacityMap[roomsCount].guests.includes(guestsCount)) {
-    addRedBorder(capacityField);
-    capacityField.setCustomValidity(roomsCapacityMap[roomsCount].errorText);
+    setInvalidMode(capacityField, roomsCapacityMap[roomsCount].errorText);
+    capacityField.reportValidity();
     return false;
   } else {
-    removeBorder(capacityField);
-    capacityField.setCustomValidity('');
+    setValidMode(capacityField);
     return true;
   }
 };
@@ -452,6 +588,11 @@ var setActiveMode = function () {
   adForm.addEventListener('submit', onAdFormSubmit);
   roomNumberField.addEventListener('change', onRoomsAndCapacityChange);
   capacityField.addEventListener('change', onRoomsAndCapacityChange);
+  titleField.addEventListener('input', onTitleInput);
+  typeField.addEventListener('change', onTypeAndPriceChange);
+  priceField.addEventListener('input', onTypeAndPriceChange);
+  timeIn.addEventListener('change', onTimeInChange);
+  timeOut.addEventListener('change', onTimeOutChange);
 };
 
 // Переводит страницу в неактивный режим
@@ -470,6 +611,11 @@ var setInactiveMode = function () {
   adForm.removeEventListener('submit', onAdFormSubmit);
   roomNumberField.removeEventListener('change', onRoomsAndCapacityChange);
   capacityField.removeEventListener('change', onRoomsAndCapacityChange);
+  titleField.removeEventListener('input', onTitleInput);
+  typeField.removeEventListener('change', onTypeAndPriceChange);
+  priceField.removeEventListener('input', onTypeAndPriceChange);
+  timeIn.removeEventListener('change', onTimeInChange);
+  timeOut.removeEventListener('change', onTimeOutChange);
 };
 
 var ads = generateAdvertisementObjects(NUMBER_OF_ADVERTISEMENT);
