9a783ff4ca92dd95df4902e0ca6a3681e00a1f0d
Anna Pronina <kiisya1@yandex.ru>
2020-06-18 16:46:53 +0300

    Правит функцию перемещения пина на более лаконичный вариант

--- js/main-pin.js
+++ js/main-pin.js
@@ -11,8 +11,6 @@
 
   var location = {
     map: {
-      x: map.offsetLeft,
-      y: map.offsetTop,
       minX: 0,
       maxX: map.offsetWidth,
       minY: 130,
@@ -20,57 +18,62 @@
     }
   };
 
-  var shift = {};
-
-  var movePin = function (evt) {
-    // координаты смещения относительно положения карты и положения точки клика на пине
-
-    var pinCoords = {
-      x: evt.clientX - location.map.x - shift.x,
-      y: evt.clientY - location.map.y - shift.y
+  var movePin = function (evt, startCoords, pinOffset) {
+    var shift = {
+      x: startCoords.x - evt.clientX,
+      y: startCoords.y - evt.clientY
     };
 
-    if (pinCoords.y + MAIN_PIN_HEIGHT_ACTIVE <= location.map.minY) {
-      pin.style.top = (location.map.minY - MAIN_PIN_HEIGHT_ACTIVE) + 'px';
-    } else if (pinCoords.y + MAIN_PIN_HEIGHT_ACTIVE >= location.map.maxY) {
-      pin.style.top = (location.map.maxY - MAIN_PIN_HEIGHT_ACTIVE) + 'px';
-    } else {
-      pin.style.top = pinCoords.y + 'px';
-    }
+    var pinCoords = {
+      x: pinOffset.x - shift.x,
+      y: pinOffset.y - shift.y
+    };
 
     if (pinCoords.x + MAIN_PIN_WIDTH / 2 <= location.map.minX) {
-      pin.style.left = (location.map.minX - MAIN_PIN_WIDTH / 2) + 'px';
+      pin.style.left = location.map.minX - MAIN_PIN_WIDTH / 2 + 'px';
     } else if (pinCoords.x + MAIN_PIN_WIDTH / 2 >= location.map.maxX) {
-      pin.style.left = (location.map.maxX - MAIN_PIN_WIDTH / 2) + 'px';
+      pin.style.left = location.map.maxX - MAIN_PIN_WIDTH / 2 + 'px';
     } else {
       pin.style.left = pinCoords.x + 'px';
     }
 
+    if (pinCoords.y + MAIN_PIN_HEIGHT_ACTIVE <= location.map.minY) {
+      pin.style.top = location.map.minY - MAIN_PIN_HEIGHT_ACTIVE + 'px';
+    } else if (pinCoords.y + MAIN_PIN_HEIGHT_ACTIVE >= location.map.maxY) {
+      pin.style.top = location.map.maxY - MAIN_PIN_HEIGHT_ACTIVE + 'px';
+    } else {
+      pin.style.top = pinCoords.y + 'px';
+    }
+
     setPinAddress(MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT_ACTIVE);
-
-  };
-
-  var onMouseMove = function (evt) {
-    evt.preventDefault();
-    movePin(evt);
-  };
-
-  var onMouseUp = function (evt) {
-    evt.preventDefault();
-    movePin(evt);
-
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
   };
 
   var onPinMousedown = function (evt) {
     if (evt.button === 0) {
       evt.preventDefault();
 
-      // координаты клика относительно левого верхнего угла пина
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
 
-      shift.x = evt.clientX - location.map.x - pin.offsetLeft;
-      shift.y = evt.clientY - location.map.y - pin.offsetTop;
+      var pinOffset = {
+        x: pin.offsetLeft,
+        y: pin.offsetTop
+      };
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+        movePin(moveEvt, startCoords, pinOffset);
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+        movePin(upEvt, startCoords, pinOffset);
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
 
       document.addEventListener('mousemove', onMouseMove);
       document.addEventListener('mouseup', onMouseUp);
